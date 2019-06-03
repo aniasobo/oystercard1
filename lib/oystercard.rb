4a22b9d58b3d28576f12d2a293fe94c233cfbd03
Aleksandra <ajmpawlik@gmail.com>
2019-06-03 15:47:27 +0100

    #4 tests iniitialised balance with a default value

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,2 +1,6 @@
 class OysterCard
+  attr_reader :balance # the presence of attr_reader :balance allows rspec and others to see @balance
+  def initialize(balance=10)
+    @balance = balance
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,10 @@
 require "oystercard"
 describe OysterCard do
+  it 'confirms its existence'do
+    expect(subject).to be
+  end
+  it 'is initilized with a default balance'do
+    oyster = OysterCard.new()
+    expect(oyster).to have_attributes(:balance => 10)
+  end
 end
